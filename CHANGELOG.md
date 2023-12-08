4baaf14e0e863dc7fe6cbfba54cc989cfd3b2a56
withcenterdev5 <withcenter.dev5@gmail.com>
2023-12-08 09:58:21 +0800

    with error Version 1.0.1 already exists.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- Addition added.
+
 ## 1.0.0
 
 - Initial version.
--- lib/src/calculator_base.dart
+++ lib/src/calculator_base.dart
@@ -1,6 +1,8 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+
+int add(int? a, int? b) {
+  if (a == null || b == null) return 0;
+  return a + b;
 }
